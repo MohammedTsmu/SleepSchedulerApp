80060d0392e60b6e4a2a5c04aabb9be8b9e83f67
Dr. Mohammed Qasim <87497268+MohammedTsmu@users.noreply.github.com>
2024-11-19 08:14:54 +0300

    Fix: bugs and errors

--- SleepSchedulerApp/Form1.Designer.cs
+++ SleepSchedulerApp/Form1.Designer.cs
@@ -3,6 +3,23 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
+
+        // Declare controls
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.Label labelEnd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.Button buttonSaveSettings;
+        private System.Windows.Forms.CheckBox checkBoxStartup;
+        private System.Windows.Forms.NumericUpDown numericUpDownRestrictionPeriod;
+        private System.Windows.Forms.Label labelRestrictionPeriod;
+        private System.Windows.Forms.Label labelLastChangeTime;
+        private System.Windows.Forms.Label labelRestrictionInfo;
+        private System.Windows.Forms.ToolTip toolTip1;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -14,22 +31,26 @@
 
         #region Windows Form Designer generated code
 
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+
+            // Initialize controls
             this.labelStart = new System.Windows.Forms.Label();
             this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.labelEnd = new System.Windows.Forms.Label();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
             this.buttonSaveSettings = new System.Windows.Forms.Button();
             this.checkBoxStartup = new System.Windows.Forms.CheckBox();
-            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
-            this.labelEnd = new System.Windows.Forms.Label();
-
-            // **New Controls**
             this.numericUpDownRestrictionPeriod = new System.Windows.Forms.NumericUpDown();
             this.labelRestrictionPeriod = new System.Windows.Forms.Label();
             this.labelLastChangeTime = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip();
             this.labelRestrictionInfo = new System.Windows.Forms.Label();
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
 
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRestrictionPeriod)).BeginInit();
             this.SuspendLayout();
@@ -43,6 +64,7 @@
             this.labelStart.Size = new System.Drawing.Size(69, 16);
             this.labelStart.TabIndex = 0;
             this.labelStart.Text = "وقت بدء النوم";
+
             // 
             // dateTimePickerStart
             // 
@@ -53,37 +75,7 @@
             this.dateTimePickerStart.Size = new System.Drawing.Size(200, 22);
             this.dateTimePickerStart.TabIndex = 1;
             this.dateTimePickerStart.ValueChanged += new System.EventHandler(this.DateTimePickerStart_ValueChanged);
-            // 
-            // buttonSaveSettings
-            // 
-            this.buttonSaveSettings.Location = new System.Drawing.Point(12, 148);
-            this.buttonSaveSettings.Name = "buttonSaveSettings";
-            this.buttonSaveSettings.Size = new System.Drawing.Size(175, 23);
-            this.buttonSaveSettings.TabIndex = 2;
-            this.buttonSaveSettings.Text = "حفظ الإعدادات";
-            this.buttonSaveSettings.UseVisualStyleBackColor = true;
-            this.buttonSaveSettings.Click += new System.EventHandler(this.buttonSaveSettings_Click);
-            // 
-            // checkBoxStartup
-            // 
-            this.checkBoxStartup.AutoSize = true;
-            this.checkBoxStartup.Location = new System.Drawing.Point(12, 177);
-            this.checkBoxStartup.Name = "checkBoxStartup";
-            this.checkBoxStartup.Size = new System.Drawing.Size(192, 20);
-            this.checkBoxStartup.TabIndex = 3;
-            this.checkBoxStartup.Text = "تشغيل التطبيق مع بدء تشغيل النظام";
-            this.checkBoxStartup.UseVisualStyleBackColor = true;
-            this.checkBoxStartup.CheckedChanged += new System.EventHandler(this.checkBoxStartup_CheckedChanged);
-            // 
-            // dateTimePickerEnd
-            // 
-            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dateTimePickerEnd.Location = new System.Drawing.Point(12, 96);
-            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
-            this.dateTimePickerEnd.ShowUpDown = true;
-            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePickerEnd.TabIndex = 4;
-            this.dateTimePickerEnd.ValueChanged += new System.EventHandler(this.DateTimePickerEnd_ValueChanged);
+
             // 
             // labelEnd
             // 
@@ -91,10 +83,30 @@
             this.labelEnd.Location = new System.Drawing.Point(12, 77);
             this.labelEnd.Name = "labelEnd";
             this.labelEnd.Size = new System.Drawing.Size(82, 16);
-            this.labelEnd.TabIndex = 0;
+            this.labelEnd.TabIndex = 2;
             this.labelEnd.Text = "وقت انتهاء النوم";
 
-            // **Configure numericUpDownRestrictionPeriod**
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(15, 96);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.ShowUpDown = true;
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerEnd.TabIndex = 3;
+            this.dateTimePickerEnd.ValueChanged += new System.EventHandler(this.DateTimePickerEnd_ValueChanged);
+
+            // 
+            // labelRestrictionPeriod
+            // 
+            this.labelRestrictionPeriod.AutoSize = true;
+            this.labelRestrictionPeriod.Location = new System.Drawing.Point(12, 141);
+            this.labelRestrictionPeriod.Name = "labelRestrictionPeriod";
+            this.labelRestrictionPeriod.Size = new System.Drawing.Size(147, 16);
+            this.labelRestrictionPeriod.TabIndex = 4;
+            this.labelRestrictionPeriod.Text = "فترة القيود (بالساعات):";
+
             // 
             // numericUpDownRestrictionPeriod
             // 
@@ -117,73 +129,71 @@
                 0,
                 0,
                 0});
+            this.toolTip1.SetToolTip(this.numericUpDownRestrictionPeriod, "مدة الوقت التي لا يمكنك فيها تغيير إعدادات النوم بعد حفظها.");
 
             // 
-            // labelRestrictionPeriod
+            // buttonSaveSettings
             // 
-            this.labelRestrictionPeriod.AutoSize = true;
-            this.labelRestrictionPeriod.Location = new System.Drawing.Point(12, 141);
-            this.labelRestrictionPeriod.Name = "labelRestrictionPeriod";
-            this.labelRestrictionPeriod.Size = new System.Drawing.Size(147, 16);
-            this.labelRestrictionPeriod.TabIndex = 6;
-            this.labelRestrictionPeriod.Text = "فترة القيود (بالساعات):";
+            this.buttonSaveSettings.Location = new System.Drawing.Point(15, 200);
+            this.buttonSaveSettings.Name = "buttonSaveSettings";
+            this.buttonSaveSettings.Size = new System.Drawing.Size(175, 23);
+            this.buttonSaveSettings.TabIndex = 6;
+            this.buttonSaveSettings.Text = "حفظ الإعدادات";
+            this.buttonSaveSettings.UseVisualStyleBackColor = true;
+            this.buttonSaveSettings.Click += new System.EventHandler(this.buttonSaveSettings_Click);
+
+            // 
+            // checkBoxStartup
+            // 
+            this.checkBoxStartup.AutoSize = true;
+            this.checkBoxStartup.Location = new System.Drawing.Point(15, 230);
+            this.checkBoxStartup.Name = "checkBoxStartup";
+            this.checkBoxStartup.Size = new System.Drawing.Size(192, 20);
+            this.checkBoxStartup.TabIndex = 7;
+            this.checkBoxStartup.Text = "تشغيل التطبيق مع بدء تشغيل النظام";
+            this.checkBoxStartup.UseVisualStyleBackColor = true;
+            this.checkBoxStartup.CheckedChanged += new System.EventHandler(this.checkBoxStartup_CheckedChanged);
 
             // 
             // labelLastChangeTime
             // 
             this.labelLastChangeTime.AutoSize = true;
-            this.labelLastChangeTime.Location = new System.Drawing.Point(12, 190);
+            this.labelLastChangeTime.Location = new System.Drawing.Point(15, 260);
             this.labelLastChangeTime.Name = "labelLastChangeTime";
             this.labelLastChangeTime.Size = new System.Drawing.Size(176, 16);
-            this.labelLastChangeTime.TabIndex = 7;
+            this.labelLastChangeTime.TabIndex = 8;
             this.labelLastChangeTime.Text = "لم يتم تعديل الإعدادات بعد.";
 
-            //
-            // ToolTip
-            //
-            // Set up the delays for the ToolTip.
-            this.toolTip1.AutoPopDelay = 5000;
-            this.toolTip1.InitialDelay = 1000;
-            this.toolTip1.ReshowDelay = 500;
-            // Force the ToolTip text to be displayed whether or not the form is active.
-            this.toolTip1.ShowAlways = true;
-            // Set up the ToolTip text for the controls
-            this.toolTip1.SetToolTip(this.numericUpDownRestrictionPeriod, "مدة الوقت التي لا يمكنك فيها تغيير إعدادات النوم بعد حفظها.");
-
-            //
-            //  label or informational text to explain how the restriction period works.
-            //
+            // 
+            // labelRestrictionInfo
+            // 
             this.labelRestrictionInfo.AutoSize = true;
-            this.labelRestrictionInfo.Location = new System.Drawing.Point(15, 185);
+            this.labelRestrictionInfo.Location = new System.Drawing.Point(15, 290);
             this.labelRestrictionInfo.Name = "labelRestrictionInfo";
             this.labelRestrictionInfo.Size = new System.Drawing.Size(400, 16);
-            this.labelRestrictionInfo.TabIndex = 8;
+            this.labelRestrictionInfo.TabIndex = 9;
             this.labelRestrictionInfo.Text = "بعد حفظ الإعدادات، لن تتمكن من تغييرها مرة أخرى حتى تنتهي فترة القيود المحددة.";
-            this.Controls.Add(this.labelRestrictionInfo);
 
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dateTimePickerEnd);
-            this.Controls.Add(this.checkBoxStartup);
-            this.Controls.Add(this.buttonSaveSettings);
+            this.ClientSize = new System.Drawing.Size(550, 330);
+            this.Controls.Add(this.labelStart);
             this.Controls.Add(this.dateTimePickerStart);
             this.Controls.Add(this.labelEnd);
-            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.labelRestrictionPeriod);
+            this.Controls.Add(this.numericUpDownRestrictionPeriod);
+            this.Controls.Add(this.buttonSaveSettings);
+            this.Controls.Add(this.checkBoxStartup);
+            this.Controls.Add(this.labelLastChangeTime);
+            this.Controls.Add(this.labelRestrictionInfo);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "جدولة النوم";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-            // **Add new controls to the form**
-            this.Controls.Add(this.numericUpDownRestrictionPeriod);
-            this.Controls.Add(this.labelRestrictionPeriod);
-            this.Controls.Add(this.labelLastChangeTime);
 
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRestrictionPeriod)).EndInit();
             this.ResumeLayout(false);
@@ -191,18 +201,5 @@
         }
 
         #endregion
-        private System.Windows.Forms.Label labelStart;
-        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
-        private System.Windows.Forms.Button buttonSaveSettings;
-        private System.Windows.Forms.CheckBox checkBoxStartup;
-        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
-        private System.Windows.Forms.Label labelEnd;
-
-        // **Declare new controls**
-        private System.Windows.Forms.NumericUpDown numericUpDownRestrictionPeriod;
-        private System.Windows.Forms.Label labelRestrictionPeriod;
-        private System.Windows.Forms.Label labelLastChangeTime;
-        private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.Label labelRestrictionInfo;
     }
 }
--- SleepSchedulerApp/Form1.cs
+++ SleepSchedulerApp/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace SleepSchedulerApp
 {
@@ -13,12 +12,6 @@ namespace SleepSchedulerApp
         // Declare timers at class level
         private Timer preSleepTimer;
         private Timer shutdownTimer;
-
-        private DateTime lastKnownSystemTime;
-        private string lastKnownTimeZoneId;
-        // Add a Timer field
-        private Timer timeZoneCheckTimer;
-
 
         public Form1()
         {
@@ -32,11 +25,10 @@ namespace SleepSchedulerApp
             dateTimePickerEnd.Value = selectedEndTime;
             checkBoxStartup.Checked = Properties.Settings.Default.RunOnStartup;
 
-            // **Load the restriction period**
-            numericUpDownRestrictionPeriod.Value = Properties.Settings.Default.RestrictionPeriod;
+            // Load the restriction period
+            numericUpDownRestrictionPeriod.Value = (decimal)Properties.Settings.Default.RestrictionPeriod;
 
-
-            // **Display the last settings change time**
+            // Display the last settings change time
             DateTime lastChange = Properties.Settings.Default.LastSettingsChangeTime;
             if (lastChange > DateTime.MinValue)
             {
@@ -47,33 +39,6 @@ namespace SleepSchedulerApp
                 labelLastChangeTime.Text = "لم يتم تعديل الإعدادات بعد.";
             }
 
-
-            // Load last known system time and time zone
-            lastKnownSystemTime = Properties.Settings.Default.LastKnownSystemTime;
-            lastKnownTimeZoneId = Properties.Settings.Default.LastKnownTimeZoneId;
-            // Check for system time or time zone changes
-            if (lastKnownSystemTime > DateTime.MinValue)
-            {
-                DetectSystemTimeManipulation();
-            }
-            else
-            {
-                // First run, initialize the values
-                lastKnownSystemTime = DateTime.Now;
-                lastKnownTimeZoneId = TimeZoneInfo.Local.Id;
-                Properties.Settings.Default.LastKnownSystemTime = lastKnownSystemTime;
-                Properties.Settings.Default.LastKnownTimeZoneId = lastKnownTimeZoneId;
-                Properties.Settings.Default.Save();
-            }
-
-            // Initialize the timer to check time zone every 5 minutes (or any interval you prefer)
-            timeZoneCheckTimer = new Timer
-            {
-                Interval = 5 * 60 * 1000 // 5 minutes in milliseconds
-            };
-            timeZoneCheckTimer.Tick += TimeZoneCheckTimer_Tick;
-            timeZoneCheckTimer.Start();
-
             // Subscribe to the SessionSwitch event
             SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
 
@@ -83,55 +48,38 @@ namespace SleepSchedulerApp
             // Subscribe to the FormClosed event
             this.FormClosed += Form1_FormClosed;
 
-            // Subscribe to system events
-            //Subscribe to the system time change event to detect changes during runtime:
-            //Subscribe to Time Zone Change Events
-            SystemEvents.TimeChanged += SystemEvents_TimeChanged;
-            
-
-
             CheckSleepTime();
         }
 
         private void DateTimePickerStart_ValueChanged(object sender, EventArgs e)
         {
             selectedStartTime = dateTimePickerStart.Value;
-            Properties.Settings.Default.StartTime = selectedStartTime;
-            Properties.Settings.Default.Save();
         }
 
         private void DateTimePickerEnd_ValueChanged(object sender, EventArgs e)
         {
             selectedEndTime = dateTimePickerEnd.Value;
-            Properties.Settings.Default.EndTime = selectedEndTime;
-            Properties.Settings.Default.Save();
         }
 
         private void checkBoxStartup_CheckedChanged(object sender, EventArgs e)
         {
-            Properties.Settings.Default.RunOnStartup = checkBoxStartup.Checked;
-            Properties.Settings.Default.Save();
-
-            string appPath = Application.ExecutablePath;
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
-            {
-                if (checkBoxStartup.Checked)
-                {
-                    key.SetValue("SleepSchedulerApp", $"\"{appPath}\"");
-                }
-                else
-                {
-                    key.DeleteValue("SleepSchedulerApp", false);
-                }
-            }
+            // Update RunOnStartup setting when saving settings
         }
 
         private void buttonSaveSettings_Click(object sender, EventArgs e)
         {
-            // Detect system time or time zone changes
-            DetectSystemTimeManipulation();
+            // Get the new sleep times from the DateTimePicker controls
+            selectedStartTime = dateTimePickerStart.Value;
+            selectedEndTime = dateTimePickerEnd.Value;
 
-            // **Check if the restriction period has passed**
+            // Validate that end time is after start time
+            if (selectedEndTime <= selectedStartTime)
+            {
+                MessageBox.Show("يجب أن يكون وقت انتهاء النوم بعد وقت بدء النوم.");
+                return;
+            }
+
+            // Check if the restriction period has passed
             DateTime lastChange = Properties.Settings.Default.LastSettingsChangeTime;
             int restrictionHours = (int)numericUpDownRestrictionPeriod.Value;
 
@@ -149,30 +97,39 @@ namespace SleepSchedulerApp
                 }
             }
 
-            // Validate that end time is after start time
-            if (selectedEndTime <= selectedStartTime)
-            {
-                MessageBox.Show("يجب أن يكون وقت انتهاء النوم بعد وقت بدء النوم.");
-                return;
-            }
-
             // Save the new sleep times
-            selectedStartTime = dateTimePickerStart.Value;
-            selectedEndTime = dateTimePickerEnd.Value;
             Properties.Settings.Default.StartTime = selectedStartTime;
             Properties.Settings.Default.EndTime = selectedEndTime;
 
-            // **Save the restriction period**
+            // Save the restriction period
             Properties.Settings.Default.RestrictionPeriod = restrictionHours;
 
-            // **Update the last settings change time**
+            // Update the last settings change time
             Properties.Settings.Default.LastSettingsChangeTime = DateTime.Now;
 
+            // Save the RunOnStartup setting
+            Properties.Settings.Default.RunOnStartup = checkBoxStartup.Checked;
+
+            // Update the registry for RunOnStartup
+            string appPath = Application.ExecutablePath;
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                if (checkBoxStartup.Checked)
+                {
+                    key.SetValue("SleepSchedulerApp", $"\"{appPath}\"");
+                }
+                else
+                {
+                    key.DeleteValue("SleepSchedulerApp", false);
+                }
+            }
+
+            // Save all settings
             Properties.Settings.Default.Save();
 
             MessageBox.Show("تم حفظ الإعدادات بنجاح.");
 
-            // **Update the last change time label**
+            // Update the last change time label
             labelLastChangeTime.Text = $"تم تعديل الإعدادات آخر مرة في: {DateTime.Now}";
 
             // Call CheckSleepTime to set up new timers
@@ -338,86 +295,6 @@ namespace SleepSchedulerApp
             }
         }
 
-        private void DetectSystemTimeManipulation()
-        {
-            try
-            {
-                DateTime currentTime = DateTime.Now;
-                string currentTimeZoneId = TimeZoneInfo.Local.Id;
-
-                // Check for significant time change (e.g., more than 5 minutes)
-                TimeSpan timeDifference = currentTime - lastKnownSystemTime;
-                if (Math.Abs(timeDifference.TotalMinutes) > 5)
-                {
-                    HandleSystemTimeChange();
-                }
-
-                // Update the stored values
-                lastKnownSystemTime = currentTime;
-                lastKnownTimeZoneId = currentTimeZoneId;
-
-                Properties.Settings.Default.LastKnownSystemTime = lastKnownSystemTime;
-                Properties.Settings.Default.LastKnownTimeZoneId = lastKnownTimeZoneId;
-                Properties.Settings.Default.Save();
-            }
-            catch (Exception ex)
-            {
-                LogEvent($"Error detecting system time manipulation: {ex.Message}");
-            }
-        }
-
-        private void HandleSystemTimeChange()
-        {
-            // Reset restriction period or take appropriate action
-            MessageBox.Show("تم اكتشاف تغيير في وقت النظام. لا يُسمح بتغيير وقت النظام أثناء تفعيل فترة القيود.",
-                            "تحذير", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
-            // Optionally, reset the last settings change time to enforce the restriction period
-            Properties.Settings.Default.LastSettingsChangeTime = DateTime.Now;
-            Properties.Settings.Default.Save();
-        }
-
-        private void HandleTimeZoneChange()
-        {
-            // Reset restriction period or take appropriate action
-            MessageBox.Show("تم اكتشاف تغيير في المنطقة الزمنية. لا يُسمح بتغيير المنطقة الزمنية أثناء تفعيل فترة القيود.",
-                            "تحذير", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
-            // Optionally, reset the last settings change time to enforce the restriction period
-            Properties.Settings.Default.LastSettingsChangeTime = DateTime.Now;
-            Properties.Settings.Default.Save();
-        }
-
-        // Implement the timer tick event handler
-        private void TimeZoneCheckTimer_Tick(object sender, EventArgs e)
-        {
-            DetectTimeZoneChange();
-        }
-
-        // Implement the DetectTimeZoneChange method
-        private void DetectTimeZoneChange()
-        {
-            try
-            {
-                string currentTimeZoneId = TimeZoneInfo.Local.Id;
-
-                if (!string.Equals(currentTimeZoneId, lastKnownTimeZoneId, StringComparison.OrdinalIgnoreCase))
-                {
-                    HandleTimeZoneChange();
-                }
-
-                // Update the stored time zone ID
-                lastKnownTimeZoneId = currentTimeZoneId;
-                Properties.Settings.Default.LastKnownTimeZoneId = lastKnownTimeZoneId;
-                Properties.Settings.Default.Save();
-            }
-            catch (Exception ex)
-            {
-                LogEvent($"Error detecting time zone change: {ex.Message}");
-            }
-        }
-
-
         private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
         {
             if (e.Reason == SessionSwitchReason.SessionUnlock)
@@ -443,10 +320,6 @@ namespace SleepSchedulerApp
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
-            SystemEvents.TimeChanged -= SystemEvents_TimeChanged;
-
-            timeZoneCheckTimer?.Stop();
-            timeZoneCheckTimer?.Dispose();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -462,16 +335,5 @@ namespace SleepSchedulerApp
                 MessageBox.Show("لا يمكنك إغلاق التطبيق خلال وقت النوم.", "جدولة النوم", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
-
-        private void SystemEvents_TimeChanged(object sender, EventArgs e)
-        {
-            DetectSystemTimeManipulation();
-        }
-
-        private void SystemEvents_TimeZoneChanged(object sender, EventArgs e)
-        {
-            DetectSystemTimeManipulation();
-        }
-
     }
 }
